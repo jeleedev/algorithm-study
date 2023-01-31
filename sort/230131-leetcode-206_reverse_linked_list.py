6e63a6ad1ecfb1928a187a922741d02354ec42c4
jeleedev <jieunlee.me@gmail.com>
2023-01-31 14:02:14 +0900

    파일명 수정

--- sort/230131-leetcode-206_reverse_linked_list.py
+++ sort/230131-leetcode-206_reverse_linked_list.py
@@ -1,11 +1,13 @@
 '''
 https://leetcode.com/problems/reverse-linked-list/
 '''
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution(object):
     def reverseList(self, head):
         node = head
